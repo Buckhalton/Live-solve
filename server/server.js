9d284fb0d5f72dc5b8295d5f9d1be0c7418119b6
Isaiah Buckhalton <Isaiah.Buckhalton@gmail.com>
2018-11-13 11:25:20 -0600

    adds index.html, /scripts, and adds server functionality.

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ let express = require('express');
 let app = express();
 const PORT = 5000;
 
-
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 app.listen(PORT, function(){
